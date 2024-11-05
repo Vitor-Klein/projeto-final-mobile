e10bd082a5f20a93036f5e7d5680001088b725b0
Vitor-Klein <vitorhklein2@gmail.com>
2024-11-05 14:27:06 -0300

    style login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,36 +3,50 @@ import { SafeAreaView, View, Text, Image, TouchableOpacity, TextInput } from 're
 
 import styles from './styles'
 
-import LogoImg from '../../assets/HomeTidyLogo.png'
+import LogoImg from '../../assets/loginImage.png'
 
 export default function Detail() {
   return (
-    <View style={styles.container}>
+    <View style={styles.backgoundContainer}>
       <Image style={styles.image} source={LogoImg} />
-      <Text style={styles.title}>Faça seu login</Text>
 
-      <SafeAreaView>
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            value={""}
-            placeholder='E-mail'
-            placeholderTextColor="#2CA58D"
-          />
+      <View style={styles.container}>
+        <Text style={styles.title}>Faça seu login</Text>
 
-          <TextInput
-            style={styles.input}
-            value={""}
-            placeholder='Senha'
-            placeholderTextColor="#2CA58D"
-          />
+        <SafeAreaView>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              value={""}
+              placeholder='E-mail'
+              placeholderTextColor="#2CA58D"
+            />
+
+            <TextInput
+              style={styles.input}
+              value={""}
+              placeholder='Senha'
+              placeholderTextColor="#2CA58D"
+            />
+          </View>
+
+          <TouchableOpacity style={styles.button} onPress={() => { }}>
+            <Text style={styles.enterButton}>Entre</Text>
+          </TouchableOpacity>
+          <Text style={styles.forgotPasswordText}>Esqueci minha senha</Text>
+        </SafeAreaView>
+
+        <View style={styles.registerContainer}>
+          <Text style={styles.registerText}>
+            Não tem conta?
+          </Text>
+          <TouchableOpacity style={styles.registerButton}>
+            <Text style={styles.registerTextGreen}>Registre-se</Text>
+          </TouchableOpacity>
         </View>
 
-        <TouchableOpacity style={styles.button} onPress={() => { }}>
-          <Text style={styles.enterButton}>Entre</Text>
-        </TouchableOpacity>
-        <Text style={styles.forgotPasswordText}>Esqueci minha senha</Text>
-      </SafeAreaView>
-    </View>
+      </View>
+    </View >
+
   )
 }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -2,15 +2,32 @@ import { StyleSheet } from 'react-native'
 import Constants from 'expo-constants'
 
 export default StyleSheet.create({
-  container: {
+  backgoundContainer: {
     flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: '#0D3C76',
+  },
+  container: {
+    width: '100%',
+    height: '75%',
     flexDirection: 'column',
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#0A2342',
+    zIndex: 0,
+  },
+  image: {
+    width: 300,
+    height: 300,
+    zIndex: 1,
+    marginStart: 100,
+    marginVertical: -100
   },
   input: {
-    width: 350,
+    width: 380,
     height: 50,
     padding: 10,
     borderRadius: 8,
@@ -20,7 +37,7 @@ export default StyleSheet.create({
   },
 
   button: {
-    width: 350,
+    width: 380,
     height: 50,
     padding: 10,
     borderRadius: 8,
@@ -32,11 +49,10 @@ export default StyleSheet.create({
   },
 
   title: {
-    fontSize: 20,
+    fontSize: 35,
     color: '#FFFDF7',
     fontWeight: 'bold',
     marginBottom: 20,
-    marginTop: 10
   },
 
   enterButton: {
@@ -50,6 +66,29 @@ export default StyleSheet.create({
     color: '#FFFDF7',
     textAlign: 'center',
     marginTop: 5
+  },
+
+  registerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  registerText: {
+    fontSize: 15,
+    color: '#FFFDF7',
+    textAlign: 'center',
+    marginTop: 5,
+  },
+  registerTextGreen: {
+    fontSize: 15,
+    color: '#2CA58D',
+    textAlign: 'center',
+    padding: 0,
+    margin: 0,
+  },
+  registerButton: {
+    marginTop: 5,
   },
 
 })
