4ab0c91231668322eee0053a3813c040a83f8a62
Fabiola Malman <Fabiolamnunes2016@gmail.com>
2024-12-12 11:30:06 -0300

    Add buttons home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,14 @@
-
-import React, { useRef } from 'react'
-import { SafeAreaView, View, Text, Image, TouchableOpacity, TextInput } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
+import React, { useRef } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import LottieView from 'lottie-react-native';
+import styles from './styles';
+import LottieView from 'lottie-react-native'; // Corrigindo a importação de LottieView
 
-import CheckListAnimation from '../../animations/CheckList.json'
-import styles from './styles'
+import CheckListAnimation from '../../animations/CheckList.json'; 
 
 export default function Home() {
-  const navigation = useNavigation();
-  const animation = useRef < LottieView > (null);
+  const animation = useRef(null); // Ref corrigido para LottieView
+
   return (
     <View style={styles.container}>
       <LinearGradient
@@ -18,23 +16,30 @@ export default function Home() {
         style={styles.background}
       >
         <View style={styles.content}>
-          <Text style={styles.title}>HomeList</Text>
+          <Text style={styles.title}>Home List</Text>
           <Text style={styles.subTitle}>O que gostaria de fazer?</Text>
-          {/* 
-          <LottieView
-            autoPlay
-            ref={animation}
-            style={{
-              width: 200,
-              height: 200,
-              backgroundColor: '#eee',
-            }}
-            source={CheckListAnimation}
-          /> */}
+
+          {/* Botões */}
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Ver Lista de Produtos</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Gerar Relatório Baixo Estoque</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Gerar Relatório De Gastos</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.footerButton}>
+            <Text style={styles.footerButtonText}>Voltar ao Onboarding</Text>
+          </TouchableOpacity>
+
         </View>
 
       </LinearGradient>
 
     </View>
-  )
+  );
 }
